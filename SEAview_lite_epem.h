0675e07206ba4e510f2093725dcf3570867b9e31
Rebecca Hicks <rjhicks@uboonegpvm01.fnal.gov>
2022-07-12 13:21:29 -0500

    Fixed issue of reco_ang. It was that I did not pass the radius in the constructor.

--- SEAview_lite_epem.h
+++ SEAview_lite_epem.h
@@ -28,7 +28,7 @@ class SEAviewer
 
 
 	//Constructor to initalize variables
-	SEAviewer(std::vector<SEAobject> &objs1, std::vector<double> &reco_vertex_3D1, std::vector<double> &reco_vertex_2D1, std::vector<double> &true_vertex1, std::string tag1, std::vector<std::string> tags1, std::vector<std::string>vals1, double radius1):objs(objs1), reco_vertex_3D(reco_vertex_3D1), reco_vertex_2D(reco_vertex_2D1), true_vertex(true_vertex1), tag(tag1), tags(tags1), vals(vals1) {}
+	SEAviewer(std::vector<SEAobject> &objs1, std::vector<double> &reco_vertex_3D1, std::vector<double> &reco_vertex_2D1, std::vector<double> &true_vertex1, std::string tag1, std::vector<std::string> tags1, std::vector<std::string>vals1, double radius1):objs(objs1), reco_vertex_3D(reco_vertex_3D1), reco_vertex_2D(reco_vertex_2D1), true_vertex(true_vertex1), tag(tag1), tags(tags1), vals(vals1), radius(radius1) {}
 
 
 
